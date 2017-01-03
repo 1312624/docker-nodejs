e635fb2ae02387a2887720946580cf421c06dd45
Minh Tri <hmtri1011@gmail.com>
2017-01-03 15:11:07 +0700

    Update

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.get('/',() => {
+    res.send('Hello world from Distelli & Docker!');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is started on: ${server.address().port}`);
+});
